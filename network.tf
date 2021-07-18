3dfccfef117f19ff9f4c358b3333f15850de843a
Pedro Abreu <pedro.schmid2311@gmail.com>
2021-07-18 14:24:44 -0300

    Created route table and associated with subnets

--- network.tf
+++ network.tf
@@ -34,3 +34,35 @@ resource "aws_subnet" "private" {
     "Name" = "private-subnet-${element(var.AVAILABILITY_ZONES, count.index)}-${var.ENVIRONMENT}"
   }
 }
+
+# Route tables for the subnets
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    "Name" = "public-route-table-${var.ENVIRONMENT}"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    "Name" = "private-route-table-${var.ENVIRONMENT}"
+  }
+}
+
+# Associating route tables to subnets
+resource "aws_route_table_association" "public" {
+  count = length(var.PUBLIC_SUBNETS_CIDR)
+
+  route_table_id = aws_route_table.public.id
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+}
+
+resource "aws_route_table_association" "private" {
+  count = length(var.PRIVATE_SUBNETS_CIDR)
+
+  route_table_id = aws_route_table.private.id
+  subnet_id      = element(aws_subnet.private.*.id, count.index)
+}
