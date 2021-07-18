85e01628fef71e701551a3d25c251c10c93021de
Pedro Abreu <pedro.schmid2311@gmail.com>
2021-07-18 14:40:51 -0300

    Created security group file and added the security group of application load balancer (ALB)

---
+++ security-group.tf
@@ -0,0 +1,27 @@
+# SG #
+resource "aws_security_group" "alb" {
+  name        = "alb-sg-${var.ENVIRONMENT}"
+  description = "Controls access to the ALB"
+  vpc_id      = aws_vpc.this.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
