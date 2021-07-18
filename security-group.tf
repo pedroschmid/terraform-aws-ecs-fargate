423acfbbf5ab343ec9330400b943373a3743e402
Pedro Abreu <pedro.schmid2311@gmail.com>
2021-07-18 14:47:59 -0300

    Added ecs security group, allowing ingress only from the alb and ssh

--- security-group.tf
+++ security-group.tf
@@ -1,9 +1,12 @@
 # SG #
+
+# ALB (Traffic Internet --> ALB)
 resource "aws_security_group" "alb" {
   name        = "alb-sg-${var.ENVIRONMENT}"
   description = "Controls access to the ALB"
   vpc_id      = aws_vpc.this.id
 
+  # HTTP
   ingress {
     from_port   = 80
     to_port     = 80
@@ -11,9 +14,40 @@ resource "aws_security_group" "alb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # HTTPS
   ingress {
     from_port   = 443
     to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# ECS (traffic ALB --> ECS || SSH --> ECS)
+resource "aws_security_group" "ecs" {
+  name        = "ecs-sg-${var.ENVIRONMENT}"
+  description = "Allow inboud access from the ALB only"
+  vpc_id      = aws_vpc.this.id
+
+  # ALB
+  ingress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    security_groups = [aws_security_group.alb.id]
+  }
+
+  # SSH
+  ingress {
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
