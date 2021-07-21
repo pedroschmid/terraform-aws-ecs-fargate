c74d9824faa88421743f0028716ae8316407317d
Pedro Abreu <pedro.abreu@vilaapps.com>
2021-07-20 21:02:53 -0300

    Refactored policies dir structure and added codebuild resource

--- codebuild.tf
+++ codebuild.tf
@@ -1,1 +1,36 @@
 # CODEBUILD #
+
+# Template file for pipeline #
+data "template_file" "buildspec" {
+  template = file("buildspec/rails.yml")
+
+  vars = {
+    IMAGE        = aws_ecr_repository.rails.repository_url
+    CLUSTER_NAME = aws_ecs_cluster.this.name
+    REGION       = var.REGION
+    ENVIRONMENT  = var.ENVIRONMENT
+  }
+}
+
+
+resource "aws_codebuild_project" "rails" {
+  name          = "rails-codebuild-${var.ENVIRONMENT}"
+  build_timeout = "10"
+  service_role  = aws_iam_role.codebuild_role.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    compute_type    = "BUILD_GENERAL1_SMALL"
+    image           = "aws/codebuild/docker:1.12.1"
+    type            = "LINUX_CONTAINER"
+    privileged_mode = true
+  }
+
+  source {
+    type      = "CODEPIPELINE"
+    buildspec = data.template_file.buildspec.rendered
+  }
+}
--- iam.tf
+++ iam.tf
@@ -1,41 +1,57 @@
 # IAM #
 
+# ECS #
 resource "aws_iam_role" "ecs_host_role" {
-  name               = "ecs_host_role"
-  assume_role_policy = file("policies/ecs-role.json")
+  name               = "ecs-host-role"
+  assume_role_policy = file("policies/ecs/ecs-role.json")
 }
 
 resource "aws_iam_role_policy" "ecs_instance_role_policy" {
-  name   = "ecs_instance_role_policy"
-  policy = file("policies/ecs-instance-role-policy.json")
+  name   = "ecs-instance-role-policy"
+  policy = file("policies/ecs/ecs-instance-role-policy.json")
   role   = aws_iam_role.ecs_host_role.id
 }
 
 resource "aws_iam_role" "ecs_service_role" {
-  name               = "ecs_service_role"
-  assume_role_policy = file("policies/ecs-role.json")
+  name               = "ecs-service-role"
+  assume_role_policy = file("policies/ecs/ecs-role.json")
 }
 
 resource "aws_iam_role_policy" "ecs_service_role_policy" {
-  name   = "ecs_service_role_policy"
-  policy = file("policies/ecs-service-role-policy.json")
+  name   = "ecs-service-role-policy"
+  policy = file("policies/ecs/ecs-service-role-policy.json")
   role   = aws_iam_role.ecs_service_role.id
 }
 
 resource "aws_iam_instance_profile" "ecs" {
-  name = "ecs_instance_profile"
+  name = "ecs-instance-profile"
   path = "/"
   role = aws_iam_role.ecs_host_role.name
 }
 
-
 resource "aws_iam_role" "ecs_execution_role" {
-  name               = "ecs_task_execution_role"
-  assume_role_policy = file("policies/ecs-task-execution-role.json")
+  name               = "ecs-task-execution-role"
+  assume_role_policy = file("policies/ecs/ecs-task-execution-role.json")
 }
 
 resource "aws_iam_role_policy" "ecs_execution_role_policy" {
-  name   = "ecs_execution_role_policy"
-  policy = file("policies/ecs-execution-role-policy.json")
+  name   = "ecs-execution-role-policy"
+  policy = file("policies/ecs/ecs-execution-role-policy.json")
   role   = aws_iam_role.ecs_execution_role.id
+}
+
+# CODEBUILD # 
+resource "aws_iam_role" "codebuild_role" {
+  name               = "codebuild-role"
+  assume_role_policy = file("policies/codebuild/codebuild_role.json")
+}
+
+data "template_file" "codebuild_policy" {
+  template = file("policies/codebuild/codebuild_policy.json")
+}
+
+resource "aws_iam_role_policy" "codebuild_policy" {
+  name   = "codebuild-policy"
+  role   = aws_iam_role.codebuild_role.id
+  policy = data.template_file.codebuild_policy.rendered
 }
